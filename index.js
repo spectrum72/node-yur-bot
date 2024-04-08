ec0a1ef220fd1e91d63668d760d55731bda8afc1
spectrum72 <spectrummentle@gmail.com>
2024-04-08 22:30:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ client.on(Discord.Events.InteractionCreate, async (interaction) => {
     if (!interaction.isCommand()) return;
 
     const command = client.commands.get(interaction.commandName);
-    const username = interaction.guild.members.fetch(interaction.user.id);
+    const user = interaction.guild.members.fetch(interaction.user.id);
     if (!command) return;
     
     await client.player.extractors.loadDefault();
@@ -80,7 +80,7 @@ client.on(Discord.Events.InteractionCreate, async (interaction) => {
 
     await interaction.deferReply();
     await command.execute(client, interaction);
-    console.log(`[${new Date()}]\n\'[${username}:${interaction.user.id}]\' -> \'${interaction.commandName}\n`);
+    console.log(`[${new Date()}]\n\'[${user.user.username}:${interaction.user.id}]\' -> \'${interaction.commandName}\n`);
 });
 
 client.on(Discord.Events.MessageCreate, async (interaction) => {
