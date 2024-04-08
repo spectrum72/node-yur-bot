1fee5e029c892f57f6700f7d21658205130a27e2
spectrum72 <spectrummentle@gmail.com>
2024-04-08 22:01:59 +0900

    Update test.js

--- Commands/test.js
+++ Commands/test.js
@@ -6,5 +6,6 @@ module.exports = {
     .setDescription("test the bot"),
     async execute(client, interaction) {
         await interaction.editReply("bot is working prop");
+        return "bot working prop";
     }
 }
