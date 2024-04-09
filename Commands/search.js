2df4db84ab69ac5af00df0058ba58a739496c132
spectrum72 <spectrummentle@gmail.com>
2024-04-09 20:21:02 +0900

    hello world

--- Commands/search.js
+++ Commands/search.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, Colors } = require("discord.js");
 const { QueryType } = require("discord-player");
 
 module.exports = {
@@ -16,6 +16,21 @@ module.exports = {
             searchEngine: QueryType.AUTO
         });
 
-        return await interaction.editReply(`${tracklist.tracks.length}`);
+        if (!tracklist.tracks.length) {
+            response.setTitle("재생 가능한 음악이 없어요!").setDescription("철자, 기호 등을 잘못 입력하였는지 확인해보세요");
+            return await interaction.editReply({embeds: [response.data]}); 
+        }
+
+        
+        let description = "";
+        for (let i = 0; i < tracklist.tracks.length; i++) {
+            track = tracklist.tracks[i];
+            description += `[${i+1}] ${track?.title}\n\n`;
+        }
+
+        response.setName(`${tracklist.tracks.length}개의 음악을 찾았어요!`)
+            .setDescription(description)
+            .setColor(Colors.Green);
+        return await interaction.editReply({ embeds : [response.data]});
     }
 }
