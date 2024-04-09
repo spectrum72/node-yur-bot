10c8b8daaab776e048402cfe6b6e3b7931f03b27
spectrum72 <spectrummentle@gmail.com>
2024-04-09 14:55:37 +0900

    search

---
+++ Commands/search.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder, EmbedBuilder, Embed } = require("discord.js");
+const { QueryType } = require("discord-player");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("search")
+        .setDescription("재생 가능한 음악을 검색합니다.")
+        .addStringOption((option) => option.setName("query").setDescription(" ")),
+    async execute(client, interaction) {
+
+        const response = new EmbedBuilder();
+
+        const query = interaction.options.getString("query");
+        const tracklist = await client.player.search(query, {
+            requestedBy: interaction.user,
+            searchEngine: QueryType.YOUTUBE_SEARCH
+        });
+
+        return await interaction.editReply(tracklist.tracks.length);
+    }
+}
